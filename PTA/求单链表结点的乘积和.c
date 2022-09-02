e8ef7c9ea61b0fffc7a7e6ee8b97b9489908b372
YeYushifdu <106903906+YeYushifdu@users.noreply.github.com>
2022-09-03 00:15:29 +0800

    Add files via upload

--- PTA/求单链表结点的乘积和.c
+++ PTA/求单链表结点的乘积和.c
@@ -1,13 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+//输入：3
+//输入：5 3 6
+//输出：846=5!+3!+6!
 
-typedef struct Node* PtrToNode;
+typedef struct Node* PtrToNode;     //Node*定义名为PtrToNode
 struct Node {
     int Data; /* 存储结点数据 */
     PtrToNode Next; /* 指向下一个结点的指针 */
 };
-typedef PtrToNode List; /* 定义单链表类型 */
+typedef PtrToNode List; /* 定义单链表类型 */       //PtrToNode定义名为List
 
 int FactorialSum(List L);
 
@@ -16,34 +19,46 @@ int main()
     int N, i;
     List L, p;
 
-    scanf("%d", &N);
+    scanf("%d", &N);        //输入个数
     L = NULL;
     for (i = 0; i < N; i++) {
-        p = (List)malloc(sizeof(struct Node));
-        scanf("%d", &p->Data);
-        p->Next = L;  L = p;
+        p = (List)malloc(sizeof(struct Node));      //p通过malloc函数新创建一个节点
+        scanf("%d", &p->Data);      //输入p.Data
+        p->Next = L;        //通过L指针可以找到原先的节点，把Next值赋为L指向的节点
+        L = p;              //L指针也指向新建的节点
     }
     printf("%d\n", FactorialSum(L));
 
     return 0;
 }
 
+
 /* 你的代码将被嵌在这里 */
 int FactorialSum(List L) {
-    int j = 0, i, e, mul = 1;
-    int FactorialSum = 0;
-    List* p = L->Next;
-    for (i = 0; i < sizeof(L); i++) {
-        while (p && j <= i) {
-            p = L->Next;
-            j++;
-            for (e = 1; e <= p; e++)
-                mul = mul * e;
-            FactorialSum += mul;
+    int Sum = 0;
+    List p = L;
+    while (p != NULL) {
+        int num = p->Data;
+        int i, mul = 1;
+        for (i = num; i > 1; i--) {
+            mul *= i;
         }
-        return FactorialSum;
+        Sum += mul;
+        p = p->Next;
     }
+    return Sum;
 }
+
+//int FactorialSum(List L) {
+//    int sum = 0;
+//    List p = L;
+//    while (1) {
+//        printf("%d\n", p->Data);
+//        if (p->Next == NULL)	break;
+//        p = (p->Next);
+//    }
+//    return sum;
+//}
 
 
 
